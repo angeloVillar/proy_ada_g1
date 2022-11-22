93782017878b3de3be3866f9d420477cee281928
angelo <angelo.villar@unmsm.edu.pe>
2022-11-22 18:20:17 -0500

    pila ++

--- src/horarios/MaxContactos.java
+++ src/horarios/MaxContactos.java
@@ -27,6 +27,57 @@ public class MaxContactos {
         return -1;
     }
 
+
+
+    static class Aux{
+        Pila aux;
+        int tiempo;
+
+        public Aux(){
+            aux = new Pila();
+            tiempo = 0;
+        }
+
+    }
+
+    public static void maxContactos(Nodo[] arr){
+        QuickSort.quickSort(arr);
+        int n = arr.length;
+
+        Aux[] dp = new Aux[n];
+        Arrays.fill(dp, new Aux());
+        dp[0].tiempo = arr[0].getTiempo();
+        dp[0].aux.push(arr[0]);
+
+        //int[] tabla = new int[n];
+        //tabla[0] = arr[0].getTiempo();
+
+        for(int i=1; i<n; i++){
+            int tiempo = arr[i].getTiempo();
+            int l = busquedaBin(arr, i);
+            if(l != -1){
+                //tiempo += tabla[l];
+                tiempo += dp[l].tiempo;
+
+                if(tiempo > dp[i-1].tiempo){
+                    dp[i].tiempo = tiempo;
+                    dp[i].aux = dp[l].aux;
+                    dp[i].aux.push(arr[i]);
+                } else {
+                    dp[i] = dp[i-1];
+                }
+
+            }
+            //tabla[i] = Math.max(tiempo, tabla[i-1]);
+
+        }
+        System.out.println("Maximo tiempo = "+dp[n-1].tiempo);
+        dp[n-1].aux.print();
+
+    }
+
+
+    /*
     static class Aux{
         Vector<Nodo> aux;
         int tiempo;
@@ -74,6 +125,7 @@ public class MaxContactos {
             System.out.println(dp[n-1].aux.get(i).getNombre());
         }
 
-    }
+    } */
+
 
 }
